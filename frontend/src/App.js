eccdd8f0b9ffdc8f84a4fb4a794fea3dbe2f4cd2
Yon Glory Kyaw <yonglorykyaw@appvantage.asia>
2021-12-15 20:03:22 +0630

    call api, done frame and nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,44 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import Container from '@mui/material/Container';
+// import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import NavMenu from './NavMenu/NavMenu';
+import axios from 'axios';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/robots')
+    .then((res)=>{
+      console.log(res)
+    })
+  }, []);
   return (
     <div className="App">
-      <h1>Robot Market</h1>
-      {/*Add your code here*/}
+      <NavMenu/>
+      <Container maxWidth="lg">
+      {/* <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}> */}
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      {/* </Box> */}
+      </Container>
     </div>
   );
 }
---
+++ frontend/src/NavMenu/NavMenu.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import './NavMenu.scss';
+export default function NavMenu() {
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"     
+          >
+            Robot Market
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box className="box"> 
+            <IconButton size="large" aria-label="show items" color="inherit">
+              <Badge badgeContent={17} color="error">
+                <LocalGroceryStoreIcon />
+              </Badge>
+              <p>฿5,300.00</p>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
